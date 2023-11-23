a65ae1b546cfaf2b0dd3a6544ddd5fe2bd1b25bc
Roman-Lviv <kituraroman@gmail.com>
2023-11-23 22:33:18 +0200

    git add .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import Profile from './components/Profile/Profile';
-import Statistics from './components/Statistics/Statistics';
-import FriendList from './components/FriendList/FriendList';
-import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
-import user from './path/to/user.json';
-import data from './path/to/data.json';
-import friends from './path/to/friends.json';
-import transactions from './path/to/transactions.json';
+import React from 'react';
+import Profile from './components/Profile/Profile'; 
+import Statistics from './components/Statistics/Statistics'; 
+import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'; 
+
+
+import user from './user.json'; 
+
+
+import data from './data.json'; 
+
+
+import friends from './friends.json'; 
+
+
+import transactions from './transactions.json'; 
 
 const App = () => {
   return (
